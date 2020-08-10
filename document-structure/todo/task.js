205694670340eca16515c84f33bc1329f1b95638
ruslanraindrop <inventmeplease@gmail.com>
2020-08-10 21:51:52 +0300

    Fifth and Sixth Homework

---
+++ document-structure/todo/task.js
@@ -0,0 +1,35 @@
+const tasksInput = document.getElementById('task__input');
+const addButton = document.getElementById('tasks__add');
+const tasksList = document.getElementById('tasks__list');
+
+function taskAdd() {
+  if (tasksInput.value) {
+    event.preventDefault();
+    tasksList.insertAdjacentHTML('beforeEnd', `
+      <div class="task">
+        <div class="task__title">
+          ${tasksInput.value}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+      </div>`
+    );
+        
+    tasksInput.value = '';
+  }
+}
+
+function taskAddFromKey(key) {
+  if (key.keyCode === 13) {
+    taskAdd();            
+  }
+}
+
+function removeTask(event) {
+  if (event.target.classList.contains('task__remove')) {
+    event.target.closest('.task').remove();
+  }
+}
+
+addButton.addEventListener('click', taskAdd);
+tasksInput.addEventListener('keydown', taskAddFromKey);
+tasksList.addEventListener('click', removeTask);
--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,16 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
+    const result = [];
+    for (let i = 0; i < this.input.options.length; i++) {
+      if (this.input.options[i].textContent.toLowerCase().includes(text.toLowerCase())) {
+        const word = {};
+        word.text = this.input.options[i].textContent;
+        word.value = this.input.options[i].value;
+        result.push(word);
       }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    }
+    return result;
   }
 }
 
